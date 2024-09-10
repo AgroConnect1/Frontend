f23d79f4448c42db747d3ea0c283dc0dacae5171
ahmedelsherbiny74-eng <ahmedelsherbiny7474@gmail.com>
2024-09-10 15:09:45 +0300

    Implement About us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,8 +11,16 @@ export default function About() {
         <header id="top" className="w-screen h-20"/>
         <Navbar currentPage={4}/>
 
-        <section className="h-screen">
-          <h1 className="text-5xl mb-5">About</h1>
+        <h1 className="text-4xl text-left mb-5 w-full ml-20">About us</h1>
+        <section className="h-fit my-5 mx-10 flex justify-between flex-col md:flex-row">
+          <div className="w-10/12 mx-auto md:w-6/12">
+            We seek improving the agricultural market locally and decrease the waste and improve the produce quality. <br /> <br />
+            Our agriculture marketplace is a web platform designed to connect farmers with consumers. By leveraging data analysis, the platform predicts which crops are in demand, reducing waste and spoilage as the market expands. <br /> <br />
+            The platform aims to provide farmers with a broader market for their produce while helping them optimize production and pricing through data-driven insights. Consumers benefit from personalized recommendations, making their shopping experience more relevant and efficient.
+          </div>
+          <img src="../../public/about-img.png" 
+            alt="About us Image" 
+            className="w-80 h-80 scale-x-[-1] mx-auto -z-10"/>
         </section>
 
         <Footer />
